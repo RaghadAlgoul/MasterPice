e4c04812fecdf32322fcb67be3c5ec0875028688
Raghad Alghoul <102190199+RaghadAlgoul@users.noreply.github.com>
2023-04-26 00:12:02 +0200

    Project

---
+++ MP(TS_SHOP)/App_Start/BundleConfig.cs
@@ -0,0 +1,88 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace MP_TS_SHOP_
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            //            "~/Scripts/jquery-{version}.js"));
+
+            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+            //            "~/Scripts/jquery.validate*"));
+
+            //// Use the development version of Modernizr to develop with and learn from. Then, when you're
+            //// ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            //            "~/Scripts/modernizr-*"));
+
+
+            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            //          "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new ScriptBundle("~/TS_SHOP/Script").Include(
+                       "~/Content/plugins/jquery/jquery.min.js",
+                       "~/Content/plugins/jquery-ui/jquery-ui.min.js",
+                       "~/Content/plugins/tether/js/tether.min.js",
+                       "~/Content/plugins/raty/jquery.raty-fa.js",
+                       "~/Content/plugins/bootstrap/dist/js/popper.min.js",
+                       "~/Content/plugins/bootstrap/dist/js/bootstrap.min.js",
+                       "~/Content/plugins/seiyria-bootstrap-slider/dist/bootstrap-slider.min.js",
+                       "~/Content/plugins/slick-carousel/slick/slick.min.js",
+                       "~/Content/plugins/jquery-nice-select/js/jquery.nice-select.min.js",
+                       "~/Content/plugins/fancybox/jquery.fancybox.pack.js",
+                       "~/Content/plugins/smoothscroll/SmoothScroll.min.js",
+                       "~/Content/js/scripts.js"
+
+
+                       ));
+
+
+
+
+
+
+            bundles.Add(new StyleBundle("~/TS_SHOP/css").Include(
+                    "~/Content/plugins/jquery-ui/jquery-ui.min.css",
+                     "~/Content/plugins/bootstrap/dist/css/bootstrap.min.css",
+                     "~/Content/plugins/font-awesome/css/font-awesome.min.css",
+                     "~/MyCss.css",
+                     "~/Content/plugins/slick-carousel/slick/slick.css",
+                     "~/Content/plugins/slick-carousel/slick/slick-theme.css",
+                     "~/Content/plugins/fancybox/jquery.fancybox.pack.css",
+
+                     "~/Content/plugins/jquery-nice-select/css/nice-select.css",
+                     "~/Content/plugins/seiyria-bootstrap-slider/dist/css/bootstrap-slider.min.css",
+                     "~/Content/css/style.css"
+                     ));
+
+            bundles.Add(new ScriptBundle("~/TS_SHOP/Script").Include(
+                        "~/Content/assets/js/jquery-1.11.0.min.js",
+                        "~/Content/assets/js/jquery-migrate-1.2.1.min.js",
+                        "~/Content/assets/js/bootstrap.bundle.min.js",
+                        "~/Content/assets/js/templatemo.js",
+                        "~/Content/assets/js/custom.js",
+                        "~/Content/assets/js/slick.min.js"
+
+
+                        ));
+
+            bundles.Add(new StyleBundle("~/PersonProfile/css").Include(
+                      "~/Content/PersonalProfile/css/style.css",
+                      "~/Content/PersonalProfile/css/style.css"
+
+                     ));
+
+
+            bundles.Add(new StyleBundle("~/Admin/css").Include(
+                     "~/Content/AdminStyle/vendor/fontawesome-free/css/all.min.css",
+                     "~/Content/AdminStyle/css/sb-admin-2.min.css"
+                     
+                    ));
+            
+        }
+    }
+}
---
+++ MP(TS_SHOP)/Controllers/HomeController.cs
@@ -0,0 +1,234 @@
+﻿using Microsoft.Ajax.Utilities;
+using Microsoft.AspNet.Identity;
+using MP_TS_SHOP_.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml.Linq;
+using System.Security.Principal;
+
+
+namespace MP_TS_SHOP_.Controllers
+{
+    public class HomeController : Controller
+    {
+        public class ViewModel
+        {
+            public IEnumerable<User> User { get; set; }
+            public IEnumerable<Comment> Comments { get; set; }
+            public IEnumerable<Product> products  { get; set; }
+            public IEnumerable<Category> categories { get; set; }
+
+        }
+        TS_SHOPEntitiess db = new TS_SHOPEntitiess();
+
+        
+        public ActionResult Home()
+        {
+            try
+            {
+                var ASPid = User.Identity.GetUserId();
+
+                var Userid = db.Users.FirstOrDefault(x => x.Id == ASPid).UserId;
+                Session["Userid"] = Userid;
+            }
+            catch { }
+            var categories = db.Categories.ToList();
+            var Myproducts = db.Products.ToList();
+            
+
+            return View(Tuple.Create(categories, Myproducts));
+
+            
+
+
+
+        }
+
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult PersonalProfile()
+        {
+
+            string id = User.Identity.GetUserId();
+            
+            ViewModel vm = new ViewModel
+            {
+                User = db.Users.Where(x => x.Id == id).ToList(),
+                Comments = db.Comments.ToList(),
+                products = db.Products.ToList()
+            };
+            return View(vm);
+        }
+
+        public ActionResult ProductOwnerProfile(int ? id)
+        {
+            ViewBag.id =id;
+            ViewModel vm2 = new ViewModel
+            {
+                User = db.Users.Where(x=>x.UserId==id).ToList(),
+                Comments = db.Comments.Where(x => x.UserId == id).ToList(),
+                products = db.Products.Where(x => x.UserID == id).ToList(),
+            };
+            return View("PersonalProfile", vm2);
+        }
+        [HttpGet]
+        public ActionResult Catigory(int ? id)
+        {
+            
+            List<int> categoriesCount = new List<int>();
+            foreach (var item in db.Categories)
+            {
+                var categoriesId = db.Categories.FirstOrDefault(z => z.CategoryName == item.CategoryName).CategoryId;
+                var count = db.Products.Where(x => x.CategoryId == categoriesId && x.State==true).Count();
+                categoriesCount.Add(count);
+
+            }
+            ViewBag.categoriesCount = categoriesCount.ToArray();
+            
+
+
+
+
+            if (id == 0 || id is string || id==null) {
+                return RedirectToAction("Home");
+            }
+            var categories = db.Categories.ToList();
+            var Myproducts = db.Products.Where(x => x.CategoryId == id).ToList();
+
+            ViewBag.categoriesName = db.Categories.FirstOrDefault(x => x.CategoryId == id).CategoryName;
+            var numberOfResult = db.Products.Where(x => x.CategoryId == id).Count();
+            string[] monthNames = new string[] {"January", "February", "March", "April","May", "June", "July", "August","September", "October", "November", "December"
+};
+            int month = DateTime.Now.Month;
+            ViewBag.Result = numberOfResult.ToString()+" Results on "+DateTime.Now.Day + monthNames[month] + " , "+ DateTime.Now.Year;
+
+            return View(Tuple.Create(categories, Myproducts));
+        }
+       
+        public ActionResult Single(int? id)
+        {
+            ViewBag.ProductOwnerImage = db.Products.FirstOrDefault(x => x.ProductId == id).User.UserImage;
+            ViewBag.ProductOwnerName = db.Products.FirstOrDefault(x => x.ProductId == id).User.Name;
+            ViewBag.ProductOwnerLocation = db.Products.FirstOrDefault(x => x.ProductId == id).User.UserLocation;
+            ViewBag.ProductOwnerId = db.Products.FirstOrDefault(x => x.ProductId == id).User.UserId;
+            return View(db.Products.Where(x => x.ProductId == id).ToList());
+        }
+
+        public ActionResult ProviderDashboard()
+        {
+            var ASPid =User.Identity.GetUserId();
+
+            var Userid = db.Users.FirstOrDefault(x=>x.Id== ASPid).UserId;
+            var obj =db.Subscriptions.SingleOrDefault(x => x.ProviderId == Userid);
+            //foreach (var item in db.Subscriptions)
+            //{
+            if (obj != null)
+            {
+
+
+                var subId = obj.SubscriptionNumber;
+                var start = obj.StartDate;
+                DateTime end1 = DateTime.Parse(start.ToString()).AddDays(2);
+                DateTime end2 = DateTime.Parse(start.ToString()).AddMonths(2);
+                DateTime end3 = DateTime.Parse(start.ToString()).AddMonths(6);
+
+                if (DateTime.Now > end1 || DateTime.Now > end2 || DateTime.Now > end3)
+                {
+                    return RedirectToAction("Home");
+
+                }
+                else
+                {
+                    ViewBag.warrning = "Hiiiiiiiii";
+                    if (subId == 1)
+                    {
+                        var x = DateTime.Now.Date.AddDays(2);
+                        if (DateTime.Now.Date.AddDays(14) == end1.Date)
+                        {
+                            ViewBag.warrning = "Last Tree Day";
+
+                        }
+
+                    }
+                    else if (subId == 2)
+                    {
+                        if (DateTime.Now.AddDays(57) == end2)
+                        {
+                            ViewBag.warrning = "Last Tree Day";
+
+                        }
+                    }
+                    else
+                    {
+                        if (DateTime.Now.AddDays(177) == end3)
+                        {
+                            ViewBag.warrning = "Last Tree Day";
+
+                        }
+                    }
+
+                }
+
+            }
+            else
+            {
+                return RedirectToAction("Subscriptions");
+            }
+
+            //    }
+
+            //}
+            
+            string id = User.Identity.GetUserId();
+            
+            ViewModel vm = new ViewModel
+            {
+                User = db.Users.Where(x => x.Id == id).ToList(),
+                Comments = db.Comments.ToList(),
+                products = db.Products.ToList()
+                
+            };
+            return View(vm);
+        }
+        public ActionResult Subscriptions()
+        {
+            return View();
+        }
+        
+
+
+        public ActionResult test(int ID)
+        {
+            
+                Subscription Obj = new Subscription();
+                Obj.SubscriptionNumber = ID;
+                Obj.StartDate = DateTime.Now;
+                var ASPid = User.Identity.GetUserId();
+                var Userid = db.Users.FirstOrDefault(x => x.Id == ASPid).UserId;
+                Obj.ProviderId = Userid;
+                db.Subscriptions.Add(Obj);
+                db.SaveChanges();
+                return RedirectToAction( "Index","ProductsProvider");
+            
+        }
+
+    }
+}
